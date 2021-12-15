104659f7f98973a5ac92eb5cd2b305cde453192c
yamodah <yassinedahlek@gmail.com>
2021-12-15 13:15:21 -0500

    added react-icons and built out general navbar structure

--- src/components/navbar/NavbarElements.js
+++ src/components/navbar/NavbarElements.js
@@ -1,24 +1,27 @@
 import styled from "styled-components"
 import {Link as LinkRouter} from "react-router-dom"
-// import {Link as LinkScroll} from "react-scroll"
+import {Link as LinkScroll} from "react-scroll"
+
+
 export const Nav = styled.nav`
 background: #000;
 height:80px;
-margin-top: -80px
+// margin-top: -80px;
 display: flex;
-align-items:center;
+justify-content: center;
+align-items: center;
 font-size: 1rem;
-postion: sticky;
+position: sticky;
 top: 0;
 z-index: 10;
 
 @media screen and (max-width: 960px){
-    transition : 0.8s all ease
+    transition: 0.8s all ease;
 }
 `
 export const NavbarContainer = styled.div`
 display:flex;
-justify-content: center;
+justify-content: space-between;
 height: 80px;
 z-index: 1;
 width: 100%;
@@ -32,7 +35,46 @@ cursor: pointer;
 font-size: 1.5rem;
 display:flex;
 align-items: center;
-margin-left 24px;
+margin-left:24px;
 font-weight: bold;
 text-decoration: none;
+`
+export const MobileIcon = styled.div`
+display:none;
+
+@media screen and (max-width: 768px){
+    display:block;
+    position: absolute;
+    top:-25px;
+    right:0;
+    transform: translate(-100%, 60%);
+    font-size: 1.8rem;
+    cursor: pointer;
+    color:#fff;
+}
+`
+export const NavMenu = styled.ul`
+display: flex;
+align-items: center;
+list-style: none;
+text-align: center;
+
+@media screen and (max-width: 768px){
+    display:none
+}
+`
+export const NavItem = styled.li`
+display:flex;
+align-items:center;
+height: 80px;
+`
+export const NavLinks = styled(LinkScroll)`
+color:#fff;
+text-decoration:none;
+padding: 0 1rem;
+cursor: pointer;
+
+&.active{
+    border-bottom:3px solid #01bf71;
+}
 `
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,13 +1,27 @@
 import React from 'react'
-import { Nav, NavbarContainer, NavLogo } from './NavbarElements'
+import {FaBars} from "react-icons/fa"
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NavbarElements'
+
 const Navbar = () => {
     return (
         <>
         <Nav>
             <NavbarContainer>
-                <NavLogo to="/">
-                    Bald-Beard
-                </NavLogo>
+                <NavLogo to="/">Bald-Beard</NavLogo>
+                <MobileIcon>
+                    <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to="about">About</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="projects">Projects</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="contact">Contact Me</NavLinks>
+                    </NavItem>
+                </NavMenu>
             </NavbarContainer>
         </Nav>
         </>
