eb9bdd8721a632ecf2130081f11232dc7384231f
yamodah <yassinedahlek@gmail.com>
2021-12-15 13:50:44 -0500

    basic sidebar structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Navbar from "./components/navbar";
+import Sidebar from "./components/sidebar";
 function App() {
   return (
     <div className="App">
+    <Sidebar />
     <Navbar/>
     <h1>Personal Site</h1>
     <p>this will inshallah be a site where I will showcase my abilities project and desires</p>
