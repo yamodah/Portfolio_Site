366764247b74d79682d714133ae654118f8c20dc
yamodah <yassinedahlek@gmail.com>
2021-12-15 11:57:48 -0500

    builidng out basic styled componenets for navbar

---
+++ src/components/navbar/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Nav, NavbarContainer, NavLogo } from './NavbarElements'
+const Navbar = () => {
+    return (
+        <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo>
+                    Bald-Beard
+                </NavLogo>
+            </NavbarContainer>
+        </Nav>
+        </>
+    )
+}
+
+export default Navbar
