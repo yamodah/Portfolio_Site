ab36bf7969db7c3bc3450b25186528d137fbe7b7
yamodah <yassinedahlek@gmail.com>
2021-12-15 14:15:13 -0500

    sidebar built out now just to make it hide

--- src/components/sidebar/SideBarElements.js
+++ src/components/sidebar/SideBarElements.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 import {FaTimes} from "react-icons/fa"
-
+import {Link as LinkScroll} from "react-scroll"
 export const SidebarContainer = styled.aside`
-postion: fixed;
+position: fixed;
 z-index:999;
 width:100%;
 height: 100%;
@@ -17,13 +17,47 @@ top: ${({isOpen})=>(isOpen ? '0' : '-100%')};
 `
 export const CloseIcon = styled(FaTimes)`
 color:#fff;
+&:hover{
+    color: rgba(232, 53, 18, 0.45);
+    transition
+}
 `
 export const Icon = styled.div`
-postition:absolute;
+position:absolute;
 top:1.2rem;
 right:1.5rem;
 background:transparent;
 font-size:2rem;
 cursor: pointer;
 outline: none;
+`
+export const SidebarWrapper = styled.div`
+color: #fff
+`
+export const SidebarMenu = styled.ul`
+display:grid;
+grid-template-columns: 1fr;
+grid-template-rows: repeat(6, 80px);
+text-align:center;
+
+@media screen and (max-width: 480px){
+    grid-template-rows: repeat(6, 60px)
+}
+`
+export const SidebarLink = styled(LinkScroll)`
+display:flex;
+align-items:center;
+justify-content:center;
+font-size:1.5rem;
+text-decoration:none;
+list-style:none;
+transition:0.2s ease-in-out;
+text-decoration:none;
+color:#fff;
+cursor:pointer;
+
+&:hover{
+    color: rgba(232, 53, 18, 0.75);
+    transition
+}
 `
--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import { SidebarContainer, Icon, CloseIcon } from './SidebarElements'
+import { SidebarContainer, Icon, CloseIcon,SidebarWrapper, SidebarLink, SidebarMenu } from './SidebarElements'
 const Sidebar = () => {
     return (
        <SidebarContainer>
            <Icon>
                <CloseIcon />
            </Icon>
+           <SidebarWrapper>
+               <SidebarMenu>
+                   <SidebarLink to="about">
+                       About
+                   </SidebarLink>
+                   <SidebarLink to="projects">
+                       Projects
+                   </SidebarLink>
+                   <SidebarLink to="contact">
+                       Contact Me
+                   </SidebarLink>
+               </SidebarMenu>
+           </SidebarWrapper>
        </SidebarContainer>
     )
 }
