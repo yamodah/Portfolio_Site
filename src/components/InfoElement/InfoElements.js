b34ace6f2bc0923f8967faca2485be6e7bc3bea7
yamodah <yassinedahlek@gmail.com>
2021-12-16 13:38:21 -0500

    built out info section still need info added new font

--- src/components/InfoElement/InfoElements.js
+++ src/components/InfoElement/InfoElements.js
@@ -9,35 +9,32 @@ background: ${({lightBg})=>(lightBg ? "#f9f9f9": "#010606")};
 }
 `
 export const InfoWrapper = styled.div`
-display:grid;
+display:flex;
 z-index:1;
 height:860px;
 max-width:1100px;
-marginright:auto;
+margin-right:auto;
 margin-left:auto;
 padding:0 24px;
 justify-content:center;
+@media and screen (max-width: 768px){
+    flex-direction:column;
+ }
 `
 export const InfoRow =  styled.div`
-display:grid;
-grid-auto-columns:minmax(auto, 1fr);
-align-items:center;
-grid0template-areas:${({imgStart})=>(imgStart ? `'col2 col1'`:`'col1 col2'`)};
-
-@media and screen (max-width: 768px){
-    grid-template-areas: ${({imgStart})=>(imgStart?`'col1' 'col2'`:`'col1 col1' 'col2 col2'`)}
-}
+font-family: 'Raleway', sans-serif;
+display:flex;
+align-items:flex-start;
+padding-top:8rem;
 `
 
 export const Column1 = styled.div`
 margin-bottom:15px;
 padding:0 15px;
-grid-area:col1;
 `
 export const Column2 = styled.div`
 margin-bottom:15px;
 padding:0 15px;
-grid-area:col2;
 `
 export const TextWrapper = styled.div`
 max-width:540px;
@@ -45,7 +42,7 @@ padding-top:0;
 padding-bottom:60px;
 `
 export const TopLine = styled.p`
-color:#01bf71;
+color:#1969EA;
 fonst-size:16px;
 line-height:16px;
 font-weight:700px;
@@ -59,7 +56,7 @@ margin-bottom:24px;
 font-size:48px;
 line-height:1.1;
 font-weight: 600;
-color:${({lightText})=>(lightText ? "#f7f8f8":"010606")}
+color:${({lightText})=>(lightText ? "#f7f8f8":"#010606")};
 
 @media screen and (max-width:480px){
     font-size:32px;
@@ -69,16 +66,18 @@ export const Subtitle = styled.p`
 max-width:440opx;
 margin-bottom:35px;
 font-size:18px;
-line:hieght:24px;
+line-height:24px;
 color:${({darkText})=>(darkText?"#010606":"#fff")}
 `
 
 export const ImgWrap = styled.div`
 max-width:555px;
-height:100%
+margin-top:4rem;
 `
 export const Img = styled.img`
 width:100%;
-margin:0 0 10px 0;
+max-height:100%;
+margin:100px 0 10px 0;
 padding-right:0;
+border-radius:50px;
 `
--- src/components/InfoElement/index.js
+++ src/components/InfoElement/index.js
@@ -36,11 +36,6 @@ const InfoSection = ({
                 <Subtitle darkText={darkText}>{description}</Subtitle>
               </TextWrapper>
             </Column1>
-            <Column2>
-              <ImgWrap>
-                <Img src={img} alt={alt} />
-              </ImgWrap>
-            </Column2>
           </InfoRow>
         </InfoWrapper>
       </InfoContainer>
