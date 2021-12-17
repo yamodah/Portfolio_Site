d12c3d994316b7fbb3a8063e7f00de73302da2d7
yamodah <yassinedahlek@gmail.com>
2021-12-17 16:05:23 -0500

    building out projects page structure

--- src/components/projects/ProjectsElements.js
+++ src/components/projects/ProjectsElements.js
@@ -1,1 +1,15 @@
 import styled from "styled-components";
+
+ export const ProjectsContainer = styled.div`
+ height:800px;
+ display:flex;
+ justify-content:center;
+ align-items:center;
+
+ @media screen and (max-width: 768px){
+     height:1100px;
+ }
+ @media screen and (max-width: 768px){
+     height:1400px;
+ }
+ `
--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Cards } from '../CardElement/Card'
+import { ProjectsContainer } from './ProjectsElements'
 const Projects = () => {
     return (
         <>
+        <ProjectsContainer>
         <Cards />
+        </ProjectsContainer>
         </>
     )
 }
