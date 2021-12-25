517c3dfe6708ddc41cd9918d25ef56128485dc27
yamodah <yassinedahlek@gmail.com>
2021-12-25 16:34:37 -0500

    added footer to index.js

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,25 +1,27 @@
-import React, {useState} from 'react'
-import HomeHero from '../heroHome'
-import Navbar from '../navbar'
-import Sidebar from '../sidebar'
-import InfoSection from '../InfoElement'
-import { homeObj } from '../InfoElement/Data'
-import Projects from '../projects'
+import React, { useState } from "react";
+import HomeHero from "../heroHome";
+import Navbar from "../navbar";
+import Sidebar from "../sidebar";
+import InfoSection from "../InfoElement";
+import { homeObj } from "../InfoElement/Data";
+import Projects from "../projects";
+import Footer from "../footer";
 
 const Home = () => {
-    const [isOpen, setIsOpen] = useState(false)
-    const toggle = () =>{
-        setIsOpen(!isOpen)
-    }
-    return (
-<>
-<Sidebar isOpen={isOpen} toggle={toggle}/>
-<Navbar toggle={toggle}/>
-<HomeHero/>
-<InfoSection {...homeObj}/>
-<Projects/>
-</>
-    )
-}
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+      <HomeHero />
+      <InfoSection {...homeObj} />
+      <Projects />
+      <Footer />
+    </>
+  );
+};
 
-export default Home
+export default Home;
