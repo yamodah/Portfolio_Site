521dafff379f91e1c3b4a8805961cfd0f373ea8e
yamodah <yassinedahlek@gmail.com>
2021-12-25 16:18:01 -0500

    builidng out super basic footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>Personal Links</FooterLinkTitle>
+              <FooterLink>Email</FooterLink>
+              <FooterLink>LinkedIn</FooterLink>
+              <FooterLink>Github</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
