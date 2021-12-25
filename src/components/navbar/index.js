fcdfb7153e4bc96398c13e4f8cbf44cd18883b4a
yamodah <yassinedahlek@gmail.com>
2021-12-25 15:57:13 -0500

    removed contact me button

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,31 +1,39 @@
-import React from 'react'
-import {FaBars} from "react-icons/fa"
-import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NavbarElements'
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+} from "./NavbarElements";
 
-const Navbar = ({toggle}) => {
-    return (
-        <>
-        <Nav>
-            <NavbarContainer>
-                <NavLogo to="home">Yassine Dahlek</NavLogo>
-                <MobileIcon onClick={toggle}>
-                    <FaBars />
-                </MobileIcon>
-                <NavMenu>
-                    <NavItem>
-                        <NavLinks to="about">About Me</NavLinks>
-                    </NavItem>
-                    <NavItem>
-                        <NavLinks to="projects">Projects</NavLinks>
-                    </NavItem>
-                    <NavItem>
+const Navbar = ({ toggle }) => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="home">Yassine Dahlek</NavLogo>
+          <MobileIcon onClick={toggle}>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About Me</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="projects">Projects</NavLinks>
+            </NavItem>
+            {/* <NavItem>
                         <NavLinks to="contact">Contact Me</NavLinks>
-                    </NavItem>
-                </NavMenu>
-            </NavbarContainer>
-        </Nav>
-        </>
-    )
-}
+                    </NavItem> */}
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
