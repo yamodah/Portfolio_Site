940de5af56e6cff1a1e4b465f9c8926a65851e45
yamodah <yassinedahlek@gmail.com>
2024-06-20 17:26:15 +0100

    rearranged project cards -- removed broken links

--- src/components/CardElement/Card.js
+++ src/components/CardElement/Card.js
@@ -25,177 +25,6 @@ export function Cards() {
     >
       <TopLine style={{ alignSelf: "flex-start" }}>Projects</TopLine>
       <Heading lightText="true" style={{ alignSelf: "flex-start" }}>
-        Notepad CLI app
-      </Heading>
-      <Card
-        style={{
-          marginBottom: "2.5rem",
-          border: "none",
-          background: "#f0ffff",
-        }}
-      >
-        <Card.Img variant="top" src={notePadCLI} alt="" />
-        <Card.Body>
-          <Card.Text>
-            An app to write notes from the terminal to a private SQL database
-            utliizing a personal Node.js api. Why you may ask ? Because why not!
-            Writing in the terminal feels cool, it feels offical. <FaNodeJs />
-            <FaReact />
-            <ImTerminal />
-            <SiPostgresql />
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer
-          style={{
-            background: "rgba(25, 105, 234, 0.5)",
-            border: "none",
-            display: "flex",
-            justifyContent: "center",
-            maxWidth: "100%",
-          }}
-        >
-          <Dropdown>
-            <Dropdown.Toggle variant="primary" id="dropdown-basic">
-              Project Links
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item
-                href="https://github.com/yamodah/notepad_cli"
-                target="_blank"
-                rel="noopener"
-              >
-                CLI App Repo
-              </Dropdown.Item>
-              <Dropdown.Item
-                href="https://github.com/yamodah/Notepad_api"
-                target="_blank"
-                rel="noopener"
-              >
-                Backend API Repo
-              </Dropdown.Item>
-              <Dropdown.Item
-                href="https://yamodahsnotepad.herokuapp.com"
-                target="_blank"
-                rel="noopener"
-              >
-                Frontend (live site)
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Card.Footer>
-      </Card>
-      <Heading lightText="true" style={{ alignSelf: "flex-start" }}>
-        We Love Movies site
-      </Heading>
-      <Card
-        style={{
-          marginBottom: "2.5rem",
-          border: "none",
-          background: "#f0ffff",
-        }}
-      >
-        <Card.Img variant="top" src={movieSite} alt="" />
-        <Card.Body>
-          <Card.Text>
-            A full stack website application built with a Postgresql database
-            Node.js backend and a react frontend. This project was undertaken
-            with the goal of practicing building and deploying RESTFUL API's .
-            We all love movies but someone has to get the data for us !{" "}
-            <FaNodeJs />
-            <FaReact />
-            <SiPostgresql />
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer
-          style={{
-            background: "rgba(25, 105, 234, 0.5)",
-            border: "none",
-            display: "flex",
-            justifyContent: "center",
-            maxWidth: "100%",
-          }}
-        >
-          <Dropdown>
-            <Dropdown.Toggle variant="primary" id="dropdown-basic">
-              Project Links
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item
-                href="https://frozen-wildwood-08033.herokuapp.com/"
-                target="_blank"
-                rel="noopener"
-              >
-                Live Site
-              </Dropdown.Item>
-              <Dropdown.Item
-                href="https://github.com/yamodah/We_Love_Movies_Project"
-                target="_blank"
-                rel="noopener"
-              >
-                Backend API Repo
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Card.Footer>
-      </Card>
-      <Heading lightText="true" style={{ alignSelf: "flex-start" }}>
-        Internal Reservations Tool
-      </Heading>
-      <Card
-        style={{
-          marginBottom: "2.5rem",
-          border: "none",
-          background: "#f0ffff",
-        }}
-      >
-        <Card.Img variant="top" src={reservationsApp} alt="" />
-        <Card.Body>
-          <Card.Text>
-            A full stack website application built with a Postgresql database
-            Node.js backend and a react frontend. This project was undertaken
-            with the goal of both simulating a tool used by front of house staff to
-            organize reservations and development/deployment of a monorepo.
-            reservatio<FaNodeJs />
-            <FaReact />
-            <SiPostgresql />
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer
-          style={{
-            background: "rgba(25, 105, 234, 0.5)",
-            border: "none",
-            display: "flex",
-            justifyContent: "center",
-            maxWidth: "100%",
-          }}
-        >
-          <Dropdown>
-            <Dropdown.Toggle variant="primary" id="dropdown-basic">
-              Project Links
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item
-                href="https://reservations-yassine.herokuapp.com/dashboard"
-                target="_blank"
-                rel="noopener"
-              >
-                Live Site
-              </Dropdown.Item>
-              <Dropdown.Item
-                href="https://github.com/yamodah/reservations_capstone"
-                target="_blank"
-                rel="noopener"
-              >
-                Monorepo
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Card.Footer>
-      </Card>
-      <Heading lightText="true" style={{ alignSelf: "flex-start" }}>
         FORREST FOSSIL FUTBOL
       </Heading>
       <Card
@@ -294,6 +123,156 @@ export function Cards() {
                 rel="noopener"
               >
                 Github Repo
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Card.Footer>
+      </Card>
+      <Heading lightText="true" style={{ alignSelf: "flex-start" }}>
+        Notepad CLI app
+      </Heading>
+      <Card
+        style={{
+          marginBottom: "2.5rem",
+          border: "none",
+          background: "#f0ffff",
+        }}
+      >
+        <Card.Img variant="top" src={notePadCLI} alt="" />
+        <Card.Body>
+          <Card.Text>
+            An app to write notes from the terminal to a private SQL database
+            utliizing a personal Node.js api. Why you may ask ? Because why not!
+            Writing in the terminal feels cool, it feels offical. <FaNodeJs />
+            <FaReact />
+            <ImTerminal />
+            <SiPostgresql />
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer
+          style={{
+            background: "rgba(25, 105, 234, 0.5)",
+            border: "none",
+            display: "flex",
+            justifyContent: "center",
+            maxWidth: "100%",
+          }}
+        >
+          <Dropdown>
+            <Dropdown.Toggle variant="primary" id="dropdown-basic">
+              Project Links
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item
+                href="https://github.com/yamodah/notepad_cli"
+                target="_blank"
+                rel="noopener"
+              >
+                CLI App Repo
+              </Dropdown.Item>
+              <Dropdown.Item
+                href="https://github.com/yamodah/Notepad_api"
+                target="_blank"
+                rel="noopener"
+              >
+                Backend API Repo
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Card.Footer>
+      </Card>
+      <Heading lightText="true" style={{ alignSelf: "flex-start" }}>
+        We Love Movies site
+      </Heading>
+      <Card
+        style={{
+          marginBottom: "2.5rem",
+          border: "none",
+          background: "#f0ffff",
+        }}
+      >
+        <Card.Img variant="top" src={movieSite} alt="" />
+        <Card.Body>
+          <Card.Text>
+            A full stack website application built with a Postgresql database
+            Node.js backend and a react frontend. This project was undertaken
+            with the goal of practicing building and deploying RESTFUL API's .
+            We all love movies but someone has to get the data for us !{" "}
+            <FaNodeJs />
+            <FaReact />
+            <SiPostgresql />
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer
+          style={{
+            background: "rgba(25, 105, 234, 0.5)",
+            border: "none",
+            display: "flex",
+            justifyContent: "center",
+            maxWidth: "100%",
+          }}
+        >
+          <Dropdown>
+            <Dropdown.Toggle variant="primary" id="dropdown-basic">
+              Project Links
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item
+                href="https://github.com/yamodah/We_Love_Movies_Project"
+                target="_blank"
+                rel="noopener"
+              >
+                Backend API Repo
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Card.Footer>
+      </Card>
+      <Heading lightText="true" style={{ alignSelf: "flex-start" }}>
+        Internal Reservations Tool
+      </Heading>
+      <Card
+        style={{
+          marginBottom: "2.5rem",
+          border: "none",
+          background: "#f0ffff",
+        }}
+      >
+        <Card.Img variant="top" src={reservationsApp} alt="" />
+        <Card.Body>
+          <Card.Text>
+            A full stack website application built with a Postgresql database
+            Node.js backend and a react frontend. This project was undertaken
+            with the goal of both simulating a tool used by front of house staff to
+            organize reservations and development/deployment of a monorepo.
+            reservatio<FaNodeJs />
+            <FaReact />
+            <SiPostgresql />
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer
+          style={{
+            background: "rgba(25, 105, 234, 0.5)",
+            border: "none",
+            display: "flex",
+            justifyContent: "center",
+            maxWidth: "100%",
+          }}
+        >
+          <Dropdown>
+            <Dropdown.Toggle variant="primary" id="dropdown-basic">
+              Project Links
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item
+                href="https://github.com/yamodah/reservations_capstone"
+                target="_blank"
+                rel="noopener"
+              >
+                Monorepo
               </Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
--- src/components/InfoElement/Data.js
+++ src/components/InfoElement/Data.js
@@ -7,11 +7,15 @@ export const homeObj = {
   description:
     "Prior to my journey as a software developer I studied philosphy. I chose philosophy for two reasons. I could study philosophy of anything! There was no boundary to what realms knowledge I could explore. I knew I would come out with a new set of tools and lenses to percieve the world and approach my problems.",
   description2:
-    "During my time studying I worked as an Audio Visual Technican supporting classroom technology and live events. From helping a professor with a projector problems during a lecture, supporting a live concert with 3 different bands, streaming audio and video feed of campus events, to producing tutorial videos, everyday brought a new challange !",
-  description4:
-    "I enjoy helping people it brings me joy and often times it scrathces that problem-solving itch too! ",
+      "The vast world of software engineering, brings with it these same challanges and oppurtunities. Everyday brings with a new challange, an opppurtunity to grow, and if I'm lucky someone to help.",
   description3:
-    "The vast world of software engineering, brings with it these same challanges and oppurtunities. Everyday brings with a new challange, an opppurtunity to grow, and if I'm lucky someone to help.",
+    "Full Stack Software Engineer with a proven track record of integrating new technologies with legacy systems, optimizing database operations, and developing scalable solutions that adhere to regulatory requirements.",
+  description4:
+    "I bring a passion for problem-solving and an ability to work under tight schedules, I leverage my extensive technical skills to enhance user engagement and streamline internal processes.",
   alt: "Me",
   darkText: true,
 };
+/*
+Software developer. Student of philosophy. Lover of knowledge. Problem solver at heart.
+
+*/
